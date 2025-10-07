0279889723a0b64797801e9867a4b4fcece92368
Opan-T <opanopan1018@gmail.com>
2025-10-07 10:02:33 +0700

    Modul_PHP

---
+++ belajarphp2.php
@@ -0,0 +1,30 @@
+<?php
+
+$a = "5";
+$b = "2.5";
+$komentar = "Selamat Datang";
+
+echo ("Nilai variabel a adalah = $a <br>");
+//variabel bertipe integer
+
+echo ("Nilai variabel b adalah = $b <br>");
+//variabel bertipe real
+
+echo ("Nilai variabel komentar adalah = $komentar<br>");
+//variabel bertipe string
+
+$tambah = $a + $b;
+$kurang = $a - $b;
+$kali = $a * $b;
+
+// rumus pembagian
+$bagi = $a / $b;
+
+
+
+echo "Hasil Penjumlahan = $tambah <br>";
+echo "Hasil Pengurangan = $kurang <br>";
+echo "Hasil Perkalian = $kali <br>";
+echo "Hasil Pembagian = $bagi <br>";
+
+?>
