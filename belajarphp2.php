ba9bee426b1cfe7f944cfbd41a0cb3a489f5122b
Opan-T <opanopan1018@gmail.com>
2025-10-07 10:15:42 +0700

    Modul_PHP

--- belajarphp2.php
+++ belajarphp2.php
@@ -1,5 +1,4 @@
 <?php
-
 $a = "5";
 $b = "2.5";
 $komentar = "Selamat Datang";
@@ -14,17 +13,29 @@ echo ("Nilai variabel komentar adalah = $komentar<br>");
 //variabel bertipe string
 
 $tambah = $a + $b;
+//rumus pengurangan
 $kurang = $a - $b;
+//rumus perkalian
 $kali = $a * $b;
-
 // rumus pembagian
 $bagi = $a / $b;
 
+echo ("Hasil penjumlahan a dan b adalah $tambah <br>");
 
+echo ("Hasil pengurangan a dan b adalah $kurang <br>");
 
-echo "Hasil Penjumlahan = $tambah <br>";
-echo "Hasil Pengurangan = $kurang <br>";
-echo "Hasil Perkalian = $kali <br>";
-echo "Hasil Pembagian = $bagi <br>";
+echo ("Hasil perkalian a dan b adalah  $kali <br>");
 
+echo ("Hasil pembagian a dan b adalah $bagi <br>");
+
+//echo "Hasil Penjumlahan = $tambah <br>";
+//echo "Hasil Pengurangan = $kurang <br>";
+//echo "Hasil Perkalian = $kali <br>";
+//echo "Hasil Pembagian = $bagi <br>";
+$nama = "ITSPKU";
+$garis = "========================================";
+echo "<p>";
+echo $garis."<br>";
+echo $komentar. " Di Lab ". $nama. "<br>Selamat Belajar Pemograman Web<br>";
+echo $garis."<br>";
 ?>
