79580df61cd8c722e212eaf298811dcdac0de867
Opan-T <opanopan1018@gmail.com>
2025-10-07 09:34:56 +0700

    Modul_PHP

---
+++ belajarphp.php
@@ -0,0 +1,10 @@
+<?php
+    $nim = '1202305005';
+    $nama = 'Muhammad Shofa Ganofi';
+    $alamat = 'Jl. tulang bawang no.IX';
+    $nilai = '90';
+    echo "NIM : ", $nim, "<br>";
+    echo "Nama : ", $nama, "<br>";
+    echo "Alamat : ", $alamat, "<br>";
+    echo "Nilai : ", $nilai, "<br>";
+?>
