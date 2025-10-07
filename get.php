314a61d95bf313f6b2753f75e64ac853ef696963
Opan-T <opanopan1018@gmail.com>
2025-10-07 11:08:56 +0700

    Modul_PHP

---
+++ get.php
@@ -0,0 +1,10 @@
+<?php
+
+$nama = $_GET['nama'];
+$nim = $_GET['nim'];
+$tanggal_lahir = $_GET['tanggal_lahir'];
+
+echo "Nama Saya $nama<br>"; 
+echo "Nim $nim<br>"; 
+echo "Tanggal Lahir $tanggal_lahir"; 
+?>
---
+++ post.php
@@ -0,0 +1,10 @@
+<?php
+
+$nama = $_POST['nama'];
+$nim = $_POST['nim'];
+$tanggal_lahir = $_POST['tanggal_lahir'];
+
+echo "Nama Saya $nama<br>"; 
+echo "Nim $nim<br>"; 
+echo "Tanggal Lahir $tanggal_lahir"; 
+?>
